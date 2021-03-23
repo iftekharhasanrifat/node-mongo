055bc824846416ab59551b6a6fcd60dfc5ac9631
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-03-23 17:18:22 +0000

    created multiple api

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send("I know how to open node")
-})
+    const fruit={
+        product :'ada',
+        price : 220
+    }
+    res.send(fruit)
+});
+
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantity:1000,price:10000})
+});
 
 app.listen(4200,()=>console.log('listening to port 4200'))
