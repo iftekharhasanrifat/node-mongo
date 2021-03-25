2c184d3eccb7a26f74d8686bde5b996b7ea8fb4f
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-03-25 07:10:44 +0000

    learned server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
 
+const users =['Rifat','Risat','Sabed','Hasan','salah']
+
+// get 
 app.get('/', (req,res)=>{
     const fruit={
         product :'ada',
@@ -11,8 +17,18 @@ app.get('/', (req,res)=>{
     res.send(fruit)
 });
 
-app.get('/fruits/banana',(req,res)=>{
-    res.send({fruit:'banana',quantity:1000,price:10000})
-});
+app.get('/users/:id',(req,res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id,name});
+})
+
+// post
+app.post('/addUser',(req,res)=>{
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
 
 app.listen(4200,()=>console.log('listening to port 4200'))
